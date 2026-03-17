949cf1ab9141095b5c018c9fbcd3dca4efdf1ff2
rukz9857 <hdrukaiya2580@gmail.com>
2026-03-17 12:17:18 +0530

    changes

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ return [
     'mail_port'     => 465,
     'mail_from'     => 'admin@metaldur.in',
     'mail_from_name'=> 'METALDUR CUTTING TOOLS',
-    'dsn'      => 'mysql:host=localhost;dbname=metaldur_erp;charset=utf8mb4',
+    'dsn'      => 'mysql:host=localhost;dbname=metaldur;charset=utf8mb4',
     'username' => 'metaldur_erp',
     'password' => 'metaldur@5253',
 ];
